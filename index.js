06c6a983d7bfa42dd4dc658e679688133242deb3
Derya Kaygisiz <derya.kaygisiz@outlook.com>
2021-05-23 16:35:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ class Formatter {
  }
 
  static sanitize(string) {
-   return string.replace(/[^A-Za-z0-9-']+/g+/s, '');
+   return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
  }
 
  static titleize(string){
    if (string[0]){
-     return string.charAt().toUpperCase();
+     return capitalize(string);
    }
    else if (string === 'a' || string === 'an' || string === 'but' || string === 'of' || string === 'and' || string === 'for' || string === 'at' || string === 'by' || string === 'from)'){
      return string;
