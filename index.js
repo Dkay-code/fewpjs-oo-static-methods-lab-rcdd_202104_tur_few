5e4d99952b7818ed45722eec60431951727da2cc
Derya Kaygisiz <derya.kaygisiz@outlook.com>
2021-05-23 16:26:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 class Formatter {
-  static capitalize(string){
-    return string.charAt(0).toUpperCase();
-  }
+  static capitalize(string) {
+   return string.charAt(0).toUpperCase() + string.slice(1);
+ }
+
+ static sanitize(string) {
+   return string.replace(/[^A-Za-z0-9-']+/g, '');
+ }
+
+ static titleize(string){
+   if string[0]{
+     return string.charAt().toUpperCase();
+   }
+   else if (string === 'a' || string === 'an' || string === 'but' || string === 'of' || string === 'and' || string === 'for' || string === 'at' || string === 'by' || string === 'from)'){
+     return string;
+   } else {
+     return string.charAt().toUpperCase();
+   }
+ }
 }
